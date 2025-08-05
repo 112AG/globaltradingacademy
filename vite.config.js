5b954353cdcfecbfffb8b37d7890c085055acf7f
Gaurav Twiari <gauravtiwari812798@gmail.com>
2025-08-05 20:59:21 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
+  plugins: [
+    tailwindcss(),
+  ],
 })
